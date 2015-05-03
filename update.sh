1b09d842e73beebe415e14de45f68ab3e4752433
Michael Snoyman <michael@snoyman.com>
2015-05-03 12:35:39 +0300

    Only one branch: hackage

--- update.sh
+++ update.sh
@@ -12,7 +12,6 @@ if [ -n "$(git status --porcelain)" ]
 then
     git add -A
     git commit -m "Update from Hackage at $(date --utc --iso=sec)" --gpg-sign=D6CF60FD
-    git push git@github.com:commercialhaskell/all-cabal-hashes.git HEAD:master
     git push git@github.com:commercialhaskell/all-cabal-hashes.git HEAD:hackage
 else
     echo No changes present
