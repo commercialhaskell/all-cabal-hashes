f27b427ddf72c76ffb760ac749b8eed8fae2e6fe
Michael Snoyman <michael@snoyman.com>
2015-05-08 09:13:26 +0300

    Tag message

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ then
     git commit -m "Update from Hackage at $(date --utc --iso=sec)" --gpg-sign=D6CF60FD
     git push git@github.com:commercialhaskell/all-cabal-hashes.git HEAD:hackage
 
-    git tag -f current-hackage -u D6CF60FD
+    git tag -f current-hackage -u D6CF60FD -m "Update from Hackage at $(date --utc --iso=sec)"
     git push --tags
 else
     echo No changes present
